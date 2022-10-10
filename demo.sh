8db88e6e52413608f6e5e93ef95a9cda6809833b
songgaojunzi <115447920+songgaojunzi@users.noreply.github.com>
2022-10-10 02:39:24 -0400

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,8 @@
+python get_news_by_year_country.py 2017 1 NI {path of masterfilelist.txt}
+python get_events_by_year_country.py 2017 1 NI {path of masterfilelist.txt}
+
+
+# python get_news_by_year_country.py 2020 1 US {path of masterfilelist.txt}
+# python get_news_by_year_country.py 2020 2 US {path of masterfilelist.txt}
+# python get_events_by_year_country.py 2020 1 US {path of masterfilelist.txt}
+# python get_events_by_year_country.py 2020 2 US {path of masterfilelist.txt}
